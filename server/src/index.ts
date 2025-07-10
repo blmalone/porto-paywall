cddc59ad5a3e780eaba9666c82b64412e0f9ec3c
Blaine Malone <blainemalone01@gmail.com>
2025-07-10 13:16:41 -0400

    fix: server changes

--- server/src/index.ts
+++ server/src/index.ts
@@ -348,44 +348,36 @@ app.get('/siwe/nonce', async (c) => {
 })
 
 app.post('/siwe', async (c) => {
-  // Extract properties from the request body and SIWE message.
   const { message, signature } = await c.req.json()
   const { address, chainId, nonce } = parseSiweMessage(message)
 
-  // If there is no nonce, we cannot verify the signature.
   if (!nonce) return c.json({ error: 'Nonce is required' }, 400)
 
-  // Check if the nonce is valid for this session.
   const nonce_session = await c.env.NONCE_STORE.get(nonce)
   if (!nonce_session) return c.json({ error: 'Invalid or expired nonce' }, 401)
-
   await c.env.NONCE_STORE.delete(nonce)
 
-  const valid = ServerActions.verifySignature(client, {
+  const validResponse = await ServerActions.verifySignature(client, {
     address: address!,
     digest: hashMessage(message),
     signature,
-  })
+  });
 
-  // If the signature is invalid, we cannot authenticate the user.
-  if (!valid) return c.json({ error: 'Invalid signature' }, 401)
+  if (!validResponse || !validResponse.valid) return c.json({ error: 'Invalid signature' }, 401)
 
-  const maxAge = 60 * 60 * 24 * 7 // 7 days
+  const maxAge = 60 * 60 * 24 * 7
   const exp = Math.floor(Date.now() / 1000) + maxAge
-
-  // Issue a JWT token for the user in a HTTP-only cookie.
   const token = await jwt.sign({ exp, sub: address }, c.env.JWT_SECRET)
 
   setCookie(c, 'auth', token, {
-    httpOnly: true,
     secure: true,
-    sameSite: 'none', // MUST be 'none' to work in cross-site iframes
+    sameSite: 'none',
     path: '/',
     maxAge,
   });
 
-  return c.json({ success: true })
-})
+  return c.json({ success: true });
+});
 
 app.post(
   '/logout',
